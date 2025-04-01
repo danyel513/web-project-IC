aece1801c6c1807e18aa0590358fbd2be2a18cd3
danyel513 <daniel.ben.cotoc@gmail.com>
2025-04-01 11:28:34 +0300

    generate structure for backend

---
+++ backend/ChromaGlamBackend/src/main/java/org/example/chromaglambackend/ChromaGlamBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.chromaglambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChromaGlamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChromaGlamBackendApplication.class, args);
+    }
+
+}
