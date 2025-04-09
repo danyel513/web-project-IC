7436a08c8bf789cb340f9138379a1605f6c36628
biab17 <bar.bianca.anemary@gmail.com>
2025-04-09 18:10:28 +0300

    final user classes

--- backend/ChromaGlamBackend/src/main/java/cg_service/UserService.java
+++ backend/ChromaGlamBackend/src/main/java/cg_service/UserService.java
@@ -7,19 +7,47 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 
-@Service
+@Service //singleton
 public class UserService
 {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
 
     public User registerUser(String name, String username,String email, String password)
     {
         String hashedPassword = PasswordEncryption.hashPassword(password);
-        User user = new User();
+        User user = new User(name,username,email,hashedPassword);
         return userRepository.save(user);
     }
 
-    public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
+    public User update(long id, String name,String username, String email, String password)
+    {
+        Optional<User> userUp = userRepository.findById(id);
+        if(userUp.isPresent())
+        {
+            User user = userUp.get();
+            user.setName(name);
+            user.setUsername(username);
+            user.setEmail(email);
+            user.setPassword(PasswordEncryption.hashPassword(password));
+            return userRepository.save(user);
+        }
+        throw new RuntimeException("User not found");
+    }
+
+    public void deleteUser(long id)
+    {
+        userRepository.deleteById(id);
+    }
+
+    public boolean authenticate(String email, String password)
+    {
+        User user = userRepository.findByEmail(email);
+        if(user == null) return false;
+        return PasswordEncryption.checkPassword(password, user.getPassword());
     }
 }
