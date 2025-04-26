f13d04dfc756afb1734f41e497bf14f36894a9b8
biab17 <bar.bianca.anemary@gmail.com>
2025-04-26 18:42:53 +0300

    register and outifit recommendation first version

--- frontend/ChromaGlamFrontend/src/app/app.component.ts
+++ frontend/ChromaGlamFrontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//import {RegisterComponent} from './register/register.component';
+import {RecommendationComponent} from './recommendation/recommendation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RecommendationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/ChromaGlamFrontend/src/app/app.routes.ts
+++ frontend/ChromaGlamFrontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component'; // <-- import your RegisterComponent
+import { RecommendationComponent } from './recommendation/recommendation.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/register', pathMatch: 'full' }, // default redirect
+ // { path: 'register', component: RegisterComponent },
+  { path: 'recommendations', component: RecommendationComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
