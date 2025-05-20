6ce3009adeede3304a641fde7748e42e96d3341f
danyel513 <daniel.ben.cotoc@gmail.com>
2025-05-20 18:11:27 +0300

    ai base connection establish

--- backend/ChromaGlamBackend/src/main/java/org/example/chromaglambackend/DAO/OutfitRepository.java
+++ backend/ChromaGlamBackend/src/main/java/org/example/chromaglambackend/DAO/OutfitRepository.java
@@ -3,11 +3,8 @@ package org.example.chromaglambackend.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface OutfitRepository extends JpaRepository<Outfit, Long> {
-
-    // method to return all outfits from the database
-    // List<Outfit> findAll();
+    // we will use the default findAll() method already implemented in JpaRepository
 }
--- backend/ChromaGlamBackend/src/main/java/org/example/chromaglambackend/DTO/OutfitController.java
+++ backend/ChromaGlamBackend/src/main/java/org/example/chromaglambackend/DTO/OutfitController.java
@@ -3,11 +3,7 @@ package org.example.chromaglambackend.DTO;
 import org.example.chromaglambackend.DAO.Outfit;
 import org.example.chromaglambackend.cg_service.OutfitService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -26,13 +22,4 @@ public class OutfitController
     public List<Outfit> getAllOutfits() {
         return outfitService.getAllOutfits();
     }
-
-
-
-    //outfit generating will call an internal service which will return the response of the model to the request
-//    @PostMapping(value = "/generate", consumes = "application/json")
-//    public ResponseEntity<HttpStatus> outfit_generating(@RequestParam int outfit_id)
-//    {
-//
-//    }
 }
